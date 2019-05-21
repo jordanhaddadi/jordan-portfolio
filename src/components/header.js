12fcdf6f5c0244b165f03d07c36079cd7e3aa0f8
Jordan <jordanhaddadi@gmail.com>
2019-05-21 11:45:29 -0400

    cleaned up code

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Card, CardBody, CardHeader, CardSubtitle, CardImg } from 'reactstrap';
-// import {Link} from 'react-router-dom';
 
 export const Header = () => {
     return (
